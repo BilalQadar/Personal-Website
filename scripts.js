472c884cecd15c86639b9707481bba29e118b1cd
Bilal Qadar <45516823+BilalQadar@users.noreply.github.com>
2019-03-08 12:07:20 -0500

    Fixed scroll error

--- scripts.js
+++ scripts.js
@@ -6,7 +6,8 @@ readMore.addEventListener('click',scrollToAbout);
 function scrollToAbout(){
   // TODO: Make this factor in current position
   var loc = aboutMe.getBoundingClientRect();
-  var top = loc.top;
+  var offset = window.pageYOffset;
+  var top = loc.top + offset;
   var left = loc.left;
   window.scroll({ top: top, left: left, behavior: 'smooth' });
 }
