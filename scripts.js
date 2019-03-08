f4f859c728256b9682a343b773598fa9be2c3a6b
Bilal Qadar <45516823+BilalQadar@users.noreply.github.com>
2019-03-08 15:24:15 -0500

    Added framework to make a fixed nav bar

--- scripts.js
+++ scripts.js
@@ -1,13 +1,26 @@
 const readMore = document.getElementById('read');
 const aboutMe = document.getElementById('about');
+const navBar = document.getElementById('navbar');
+var navBarLoc = navBar.offsetTop;
 
 readMore.addEventListener('click',scrollToAbout);
 
 function scrollToAbout(){
-  // TODO: Make this factor in current position
   var loc = aboutMe.getBoundingClientRect();
   var offset = window.pageYOffset;
   var top = loc.top + offset;
   var left = loc.left;
   window.scroll({ top: top, left: left, behavior: 'smooth' });
+}
+
+window.onscroll = scrollNav;
+
+function scrollNav(){
+  // TODO: Make this add a fixed class
+  pageOffset = window.pageYOffset;
+  if(pageOffset > navBarLoc){
+    navBar.classList.add('nav-bar-st');
+  } else{
+    navBar.classList.remove('nav-bar-st');
+  }
 }
